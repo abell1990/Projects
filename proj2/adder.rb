eff8b6184425d2bb206ef6ee363d04f01630b99f
Rafael Moreno Ferrer <rmferrer@gmail.com>
2012-07-21 19:09:00 -0700

    Done with part 6/6: problem 6.

---
+++ proj2/adder.rb
@@ -0,0 +1,16 @@
+class Adder
+  def initialize(value)
+    @value = value
+  end
+
+  def method_missing(symbol, *args)
+    methodName = symbol.id2name
+    if methodName =~ /^plus(\d+)$/
+      method_definition = "def "+methodName+"() return @value + " + $1 + " end"
+      self.class.class_eval(method_definition)
+      eval methodName
+    else
+      super.method_missing(symbol, *args)
+    end
+  end
+end
