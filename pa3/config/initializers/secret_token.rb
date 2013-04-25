a49fd68138445d81e173207bb5029b4dab17ba28
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-04-24 22:46:25 -0700

    initial files for pa3

---
+++ pa3/config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Pa3::Application.config.secret_token = '7a5cf1dec62ee4b14473e828db2a25838b0bbcb750cc2008ab5df17b67e4f5852ad8530756e0fa8d3b97672c35cea63fcba716a1cc354f1ddbc96c3b8bad9cdd'
