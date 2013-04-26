69f3895570ca0feba4dd66185d831f6f4ae5f376
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-04-26 14:41:06 -0700

    PA4: Added a full_name method to users model to simplify views

--- pa4/app/models/user.rb
+++ pa4/app/models/user.rb
@@ -2,4 +2,8 @@ class User < ActiveRecord::Base
   attr_accessible :first_name, :last_name
   has_many :photos
   has_many :comments
+
+  def full_name
+  	self.first_name + " " + self.last_name
+  end
 end
