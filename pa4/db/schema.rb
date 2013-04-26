970b173fe16eda44f1641c98fecf9023ce4cc2a3
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-04-26 04:16:55 -0700

    PA4: adding db schema

--- pa4/db/schema.rb
+++ pa4/db/schema.rb
@@ -11,7 +11,20 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20130426095253) do
+ActiveRecord::Schema.define(:version => 20130426101854) do
+
+  create_table "comments", :force => true do |t|
+    t.integer  "photo_id"
+    t.integer  "user_id"
+    t.datetime "date_time"
+    t.text     "comment"
+  end
+
+  create_table "photos", :force => true do |t|
+    t.integer  "user_id"
+    t.datetime "date_time"
+    t.string   "file_name"
+  end
 
   create_table "sessions", :force => true do |t|
     t.string   "session_id", :null => false
@@ -22,5 +35,10 @@ ActiveRecord::Schema.define(:version => 20130426095253) do
 
   add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
   add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
+
+  create_table "users", :force => true do |t|
+    t.string "first_name"
+    t.string "last_name"
+  end
 
 end
