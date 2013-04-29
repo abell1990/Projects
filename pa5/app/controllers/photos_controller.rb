bbae9664287f4d1ffe0f296e32a1899dd5718aa0
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-04-29 05:56:18 -0700

    PA5: Added login action/view, logout, added logic to each action method to require login

--- pa5/app/controllers/photos_controller.rb
+++ pa5/app/controllers/photos_controller.rb
@@ -1,7 +1,12 @@
 class PhotosController < ApplicationController
 
   def index
-  	if params[:id] and User.exists?(params[:id])
+    if !session[:current_user_id]
+      flash[:login_info] = "Please login to see this content."
+      redirect_to(:controller => :users, :action => :login)
+    end
+
+    if params[:id] and User.exists?(params[:id])
   		@user = User.find(params[:id])
   	end
   end
--- pa5/app/controllers/users_controller.rb
+++ pa5/app/controllers/users_controller.rb
@@ -1,7 +1,40 @@
 class UsersController < ApplicationController
   
   def index
+    if !session[:current_user_id]
+      flash[:login_info] = "Please login to see this content."
+      redirect_to(:controller => :users, :action => :login)
+    end
+
   	@all_users = User.all
+  end
+
+  def login
+    if session[:current_user_id]
+      redirect_to(:controller => :photos, :action => :index, :id => session[:current_user_id])
+    end
+  end
+
+  def logout
+    if session[:current_user_id]
+      flash[:login_success] = "You have successfully logged out."
+    end
+
+    session[:current_user_id] = nil
+    redirect_to(:controller => :users, :action => :login)
+  end
+
+  def post_login
+    user = User.find_by_login(params[:login])
+
+    if user
+      session[:current_user_id] = user.id
+      redirect_to(:controller => :photos, :action => :index, :id => user.id)
+    else
+      flash[:login_error] = "Invalid username."
+      render(:controller => :users, :action => :login)
+    end
+
   end
 
 end
