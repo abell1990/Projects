81a10681b62cfbe7afc6114f13185b2b5c557b9c
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-05-02 23:25:04 -0700

    PA2: Correcting mistake. Group by all characters no just a-z

--- pa2/group.rb
+++ pa2/group.rb
@@ -1,10 +1,12 @@
 module Enumerable
   def group_by_first_letter
     groups = {}
-    ("a".."z").each do |letter|
-      starts_with = self.find_all{|elem| elem[0] == letter}
+    # iterate over all characters and group by first character in string
+    (0..255).each do |charNo|
+      char = charNo.chr # convert number to character
+      starts_with = self.find_all{|elem| elem[0] == char}
       if starts_with.length > 0
-        groups[letter] = starts_with
+        groups[char] = starts_with
       end
     end
     return groups
@@ -20,6 +22,13 @@ end
 
 
 x = ["abcd", "axyz", "able", "xyzab", "qrst"]
+x.each_group_by_first_letter do |letter, words|
+  printf("%s: %s\n", letter, words.join(" "));
+end
+
+puts "\n"
+
+x = ["Abcd", "axyz", "Able", "xyzab", "qrst", "alamo", "1Z2D", "Xy"]
 x.each_group_by_first_letter do |letter, words|
   printf("%s: %s\n", letter, words.join(" "));
 end
