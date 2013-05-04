74f485f28aa5c07e3cf0c56afde24e25642db33f
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-05-04 02:01:13 -0700

    PA5: fixing bug where rake db:migrate:reset would set login column to all nils

--- pa5/db/migrate/20130428211924_add_login_to_users.rb
+++ pa5/db/migrate/20130428211924_add_login_to_users.rb
@@ -2,7 +2,9 @@ class AddLoginToUsers < ActiveRecord::Migration
   def change
     add_column :users, :login, :string
 
-    # Iniialize login attribute to downcased user's last name
+    User.reset_column_information
+
+    # Initialize login attribute to downcased user's last name
     User.all.each do |user|
     	user.update_attributes(:login => user.last_name.downcase)
     end
