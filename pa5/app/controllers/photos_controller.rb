b578f641eb5fcc3586711fcb34cf7698eaff65bb
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-05-04 05:55:46 -0700

    PA5:  Validating login in right places for photos and users controller
    Allow everyone to see user index and photo index for each user. In case
    of photo index for a user only display add comment link to logged in
    users

--- pa5/app/controllers/photos_controller.rb
+++ pa5/app/controllers/photos_controller.rb
@@ -1,11 +1,14 @@
 class PhotosController < ApplicationController
 
+  # URL access: anyone
+  # HTTP method: GET
   def index
-    if !session[:current_user_id]
-      flash[:login_info] = "Please login to see this content."
-      redirect_to(:controller => :users, :action => :login)
+    # check if whoever made request is logged in
+    if session[:current_user_id]
+      @logged_in = true
     end
 
+    # find user they are looking for (if exists)
     if params[:id] and User.exists?(params[:id])
   		@user = User.find(params[:id])
   	end
--- pa5/app/controllers/users_controller.rb
+++ pa5/app/controllers/users_controller.rb
@@ -1,20 +1,22 @@
 class UsersController < ApplicationController
-  
-  def index
-    if !session[:current_user_id]
-      flash[:login_info] = "Please login to see this content."
-      redirect_to(:controller => :users, :action => :login)
-    end
 
+  # URL access: anyone
+  # HTTP method: GET
+  def index
   	@all_users = User.all
   end
 
+  # URL access: anyone
+  # HTTP method: GET
   def login
+    # if whoever made request is logged in already redirect to their photos page
     if session[:current_user_id]
       redirect_to(:controller => :photos, :action => :index, :id => session[:current_user_id])
     end
   end
 
+  # URL access: anyone
+  # HTTP method: GET
   def logout
     if session[:current_user_id]
       flash[:login_success] = "You have successfully logged out."
@@ -24,13 +26,22 @@ class UsersController < ApplicationController
     redirect_to(:controller => :users, :action => :login)
   end
 
+  # Action that handles HTTP POST requests to login users
+  # URL access: anyone
+  # HTTP method: POST
   def post_login
+
+    # don't allow HTTP GET requests to this URL, redirect to login page
+    if !validate_http_post(:users, :login)
+      return
+    end
+
     user = User.find_by_login(params[:login])
 
-    if user
+    if user  # valid login, proceed to their photos page
       session[:current_user_id] = user.id
       redirect_to(:controller => :photos, :action => :index, :id => user.id)
-    else
+    else     # invalid login, go back to login page with error msg
       flash[:login_error] = "Invalid username."
       render(:controller => :users, :action => :login)
     end
