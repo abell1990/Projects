1bcf0f86a00511defd6a20cb2bdf00dac22e4359
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-05-04 05:48:58 -0700

    PA5: wrote login and http post/get validations helpers in application_controller

--- pa5/app/controllers/application_controller.rb
+++ pa5/app/controllers/application_controller.rb
@@ -1,5 +1,35 @@
 class ApplicationController < ActionController::Base
+
   protect_from_forgery
 
   $project_number = 5
+
+  def validate_login
+    if !session[:current_user_id]
+      flash[:login_info] = "You must be logged in to access this content."
+      redirect_to(:controller => :users, :action => :login)
+      return false
+    end
+
+    return true
+  end
+
+  def validate_http_get(controller, action)
+    if request.post?
+      redirect_to(:controller => controller, :action => action)
+      return false
+    end
+
+    return true
+  end
+
+  def validate_http_post(controller, action)
+    if request.get?
+      redirect_to(:controller => controller, :action => action)
+      return false
+    end
+
+    return true
+  end
+
 end
