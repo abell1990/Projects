382c8a2273eebe2a097d4e59e8925ba15fe4f265
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-05-23 23:25:59 -0700

    PA2: factored tablescan.js

--- pa6/TableScan.js
+++ pa6/TableScan.js
@@ -1,48 +1,47 @@
-function TableScan() {}
+function TableScan () {}
 
-TableScan.sumColumn = function(id, colName)
+TableScan.sumColumn = function (id, colName)
 {
-	var table = document.getElementById(id);
+	var table = document.getElementById (id);
 
-	// TODO: factor out?
-	// sanity checks
-	if (!table) return 0;
-	if (table.nodeName != "TABLE") return 0;
-	if (!table.firstElementChild || table.firstElementChild.nodeName != "TBODY") return 0;
-	if (!table.firstElementChild.firstElementChild || table.firstElementChild.firstElementChild.nodeName != "TR") return 0;
+	if (!TableScan.isValidTable (table)) return 0;
 
-	var tbody = table.firstElementChild;
+	var index = TableScan.getColumnIndex (table, colName);
 
-	// find the column index for colName
-	var headerRow = tbody.firstElementChild;
-	var headerCols = headerRow.children;
-	var index = -1;
-	for (var i = 0; i < headerCols.length; i++)
-	{
-		if (headerCols[i].textContent == colName)
-			index = i;
-	}
-
-	// Colname not found
+	/* colName not found */
 	if (index == -1) return 0;
 
-	// sum up values over all data rows for that column
+	/* sum up all numeric values values over all data rows for that column */
+	var allRows = table.rows; 
 	var sum = 0;
-	var allRows = tbody.children; // TODO: use rows method
-	for(var i = 1; i < allRows.length; i++)
+	for (var i = 1; i < allRows.length; i++)
 	{
-		var cell = allRows[i].children[index]; // TODO: use cells method		
+		var cell = allRows[i].children[index];		
+		
 		if (!cell) continue;
 		
 		var cellValue = +cell.textContent;
 		
-		if (TableScan.isNumber(cellValue))
+		if (TableScan.isNumber (cellValue))
 		{
 			sum += cellValue;
 		}
 	}
 
-	return Math.round(sum * 1E6) / 1E6;
+	return Math.round (sum * 1E6) / 1E6;
+}
+
+
+
+/* Helper method section */
+
+TableScan.isValidTable = function (table)
+{
+	if (!table) return false; /* check element exists */
+	if (table.nodeName != "TABLE") return false; /* is it a table element */
+	if (!table.tBodies[0] || !table.rows[0]) return false; /* verify it has a tbody and a header row*/
+
+	return true;
 }
 
 TableScan.isNumber = function (n) 
@@ -50,9 +49,18 @@ TableScan.isNumber = function (n)
 	return !isNaN(parseFloat(n)) && isFinite(n); 
 }
 
+/* Returns index of column whose name is colName or -1 if not found */
+TableScan.getColumnIndex = function (table, colName)
+{
+	var headerRow = table.rows[0];
+	var headerCols = headerRow.children;
+	var index = -1;
+	
+	for (var i = 0; i < headerCols.length; i++)
+	{
+		if (headerCols[i].textContent == colName)
+			index = i;
+	}
 
-// TODO: check all vars are local (i.e. declared with var keyword)
-// Q: can I assume all tables will be complete? i.e. have the same number of cells per row. If not how to handle it?
-// should we worry about precision problems in the sum?  problems with TableScan.sumColumn("table2", "Unit Price") and TableScan.sumColumn("table1", "Weight")
-// use tagname intead of nodename. validate that cells are tds
-// use rows, cells methods on table
+	return index;
+}
