4bfa5353b4847a14b0b55eca880f688654b0c6f5
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-05-26 19:00:45 -0700

    PA7: added validations for tag.rb model

--- pa7/app/models/tag.rb
+++ pa7/app/models/tag.rb
@@ -1,6 +1,11 @@
 class Tag < ActiveRecord::Base
-  attr_accessible :height, :photo_id, :user_id, :width, :x_coord, :y_coord
+  attr_accessible :photo_id, :user_id, :x_coord, :y_coord, :height, :width
 
-  belongs_to :user
   belongs_to :photo
+  belongs_to :user
+
+  validates :photo_id, :user_id, :x_coord, :y_coord, :height, :width, :presence => true
+  validates :x_coord, :y_coord, :numericality => { :only_integer => true, :greater_than_or_equal_to => 0 }
+  validates :height, :width, :numericality => { :only_integer => true, :greater_than => 0 }
+  validates :photo, :user, :presence => true
 end
